71e35efebe5bf04adcebbd581e55cc7f69b63487
Joakym Girard <joakymgirard@msn.com>
2021-11-20 23:23:53 -0500

    Nav-bar & Hero Section frame done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ProjectSectionComponent } from './project-section/project-section.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ProjectSectionComponent,
+    HeroSectionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
